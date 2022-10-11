54e2a53e35e30e3103498551468a58813fd8bc49
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-10-10 23:22:46 -0300

    Creacion y migracion de los modelos y enlazo urls de la app

--- Biblioteca/models.py
+++ Biblioteca/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Libros(models.Model):
+    autor = models.CharField(max_length=40)
+    titulo = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.titulo} de {self.autor}"
+
+class Empleados(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+
